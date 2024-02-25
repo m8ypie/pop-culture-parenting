a7db8d01d7aa0de50bef93b71f002bbd59eafe39
Joel Berta <joel@humanitix.com.au>
2024-02-25 16:38:08 +1100

    disable sort for now, lazy loading breaks when order changes

--- website/src/routes/episodes/+page.svelte
+++ website/src/routes/episodes/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   import ContentPage from "$lib/components/ContentPage.svelte";
   import Episode from "$lib/components/Episode.svelte";
-  import Sort from "$lib/components/Sort.svelte";
+  // import Sort from "$lib/components/Sort.svelte";
   import Logo from "../../assets/Logo.svg.svelte"
   import podcastList from "../../assets/podcasts.json"
 
   let descending = true
   let podcasts = podcastList.podcasts
 
-  function changeOrder() {
-    descending = !descending
-    podcasts = podcasts.reverse()
-  }
+  // function changeOrder() {
+  //   descending = !descending
+  //   podcasts = podcasts.reverse()
+  // }
 
 </script>
 
@@ -19,9 +19,9 @@
 <ContentPage pageName="episodes">
   <Logo slot="filler"/>
   <div slot="content">
-    <div style="width: 100%; display:flex; flex-direction:row-reverse">
+    <!-- <div style="width: 100%; display:flex; flex-direction:row-reverse">
       <Sort on:click={changeOrder} descending={descending}/>
-    </div>
+    </div> -->
     <div style="gap:35px; display:flex; flex-direction:column">
       {#each podcasts as episode }
         <Episode episode={episode}/>
