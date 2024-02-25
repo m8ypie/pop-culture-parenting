f329392ffc0034954ee86111082761e8d6677668
Joel Berta <joel@humanitix.com.au>
2024-02-25 12:55:15 +1100

    add transitions to pages

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -2,6 +2,19 @@
   import '@fontsource/audiowide';
   import '../fonts.css'
   import '../styles.css'
+
+  import { onNavigate } from '$app/navigation';
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+});
 </script>
 
 <slot />
